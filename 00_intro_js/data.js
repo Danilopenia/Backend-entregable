4b81ff14487098a7e3298a3012d486373decee9f
danilopenia <danilopenia14@gmail.com>
2023-12-11 19:13:44 -0300

    00introjs

---
+++ 00_intro_js/data.js
@@ -0,0 +1,15 @@
+const myName = "dani"
+let age = 21
+const price = 100
+const list = ["wandavision","harry potter","pokemon","el secreto de sus ojos"]
+
+console.log(myName);
+console.log(age);
+console.log(price);
+console.log(list);
+
+age++
+console.log(age);
+
+list.push("naruto")
+console.log(list);
