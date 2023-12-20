0b290329999638030882c372b259deaf2cc41af9
Danilopenia <148593832+Danilopenia@users.noreply.github.com>
2023-12-19 22:05:11 -0300

    entregable2 (#3)

--- Entregable/productManager.js
+++ Entregable/productManager.js
@@ -1,58 +1,82 @@
 class ProductManager {
-    static #products = [];
-  
-    id;
-    title;
-    photo;
-    price;
-    stock;
-  
-    create(data) {
-      const propsList = ["title", "photo", "price", "stock"];
-      const keyList = Object.keys(data);
-  
-      const missingProps = [];
-  
-      for (let i = 0; i < propsList.length; i++) {
-        !propsList.includes(keyList[i]) ? missingProps.push(propsList[i]) : null;
-      }
-  
-      if (missingProps.length) {
-        console.log(`faltan las propiedades: ${missingProps.join(" ")}`);
-      } else {
-        const id =
-          ProductManager.#products[ProductManager.#products.length - 1]?.id + 1 ||
-          1;
-  
-        ProductManager.#products.push({ id, ...data });
-      }
-    }
-  
-    getProducts() {
-      return ProductManager.#products;
-    }
-  
-    getProductById(id) {
-      return ProductManager.#products.find((el) => el.id === id);
+  static #products = [];
+  static #events=[];
+
+  id;
+  title;
+  photo;
+  price;
+  stock;
+
+  create(data) {
+    const requiredProps = ["title", "photo", "price", "stock"];
+    const missingProps = requiredProps.filter((prop) => !(prop in data));
+
+    if (missingProps.length) {
+      console.log(`faltan las propiedades: ${missingProps.join(" ")}`);
+    } else {
+      const id =
+        ProductManager.#products[ProductManager.#products.length - 1]?.id + 1 ||
+        1;
+
+      ProductManager.#products.push({ id, ...data });
     }
   }
+// return ProductManager.#products;
+  getProducts() {
+    try {
+     const allProducts = ProductManager.#products;
+     if (allProducts.length===0) {
+       throw new Error("they arent products");
+     }else{
+       return allProducts;
+     }
+     
+    } catch (error) {
+     return error.message
+    }
+   }
+
+ getProductById(id) {
+try {
+  let one = ProductManager.#products.find((el) => el.id ===id);
+   if (one) {
+   return one; 
+   }else{
+    throw new Error ("ERROR the product with id "+id+" doesnt exist");
+   }
   
-  const ProdtManager = new ProductManager();
-  
-  ProdtManager.create({
-    title: "bolsa de consorcio",
-    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRay89um-pAfJd5Z8kMVZOKDSKdr9wWuUuVJw&usqp=CAU",
-    price: 500,
-    stock: 1500,
-  });
-  
-  console.log(ProdtManager.getProductById(1));
-  
-  ProdtManager.create({
-    title: "bolsa camiseta",
-    photo: "https://tienda.papeleradelmar.com.ar/2982-large_default/bolsa-camiseta-20x30-reforzada.jpg",
-    
-  });
-  
-  console.log(ProdtManager.getProducts());
-  
+  } catch (error) {
+  return error.message;
+}
+}
+}
+
+const ProdtManager = new ProductManager();
+
+ProdtManager.create({
+  title: "bolsa de consorcio",
+  photo:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRay89um-pAfJd5Z8kMVZOKDSKdr9wWuUuVJw&usqp=CAU",
+  price: 500,
+  stock: 1500,
+});
+
+ProdtManager.create({
+  title: "bolsa camiseta",
+  photo:
+    "https://tienda.papeleradelmar.com.ar/2982-large_default/bolsa-camiseta-20x30-reforzada.jpg",
+    price: 700,
+    stock:2000
+});
+console.log(ProdtManager.getProducts(1));
+console.log(ProdtManager.getProductById(10));
+
+ProdtManager.create({
+  title: "bolsa camiseta",
+  photo:
+    "https://tienda.papeleradelmar.com.ar/2982-large_default/bolsa-camiseta-20x30-reforzada.jpg",
+   
+});
+
+
